17762141ee3aa34c43320459e4a6b6076b4d35f0
alexander.akelyev <alexander.akelev@gmail.com>
2022-05-07 23:23:33 +0400

    fix: z-index on layout

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 
-import './index.scss';
-import { theme } from './styles/theme';
 import App from './App';
+
+import { theme } from './styles/theme';
+import './styles/main.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
--- src/layout/Layout/index.tsx
+++ src/layout/Layout/index.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 import NavMenu from '../../components/NavMenu';
 import SearchPanel from '../../components/SearchPanel';
+import Tweet from '../../components/Tweet';
 
 import './Layout.scss';
 
@@ -25,7 +26,20 @@ const Layout: React.FC = (): React.ReactElement => {
           <div className="sticky-top">
             <SearchPanel />
           </div>
-          <div className="layout-right__content" />
+          <div className="layout-right__content">
+            <Tweet
+              user={{
+                nickname: 'Dan',
+                author: 'dan_abramov',
+                avatarUrl:
+            'https://external-preview.redd.it/yCIdRVIdKSZlwTacwwnrqx6o-ou1GNYc-WI24p_w6xE.jpg?auto=webp&s=222ab63c87addc7117b32dcbaee992fbfc60aefc',
+              }}
+              text={`“when people are rude to you, don’t forget that they might be cranky
+          for an entirely unrelated reason that you can’t influence and they
+          just happen to (intentionally or not) take it out on you” — me, a
+          person who most definitely never does that`}
+            />
+          </div>
         </div>
       </div>
     </section>
