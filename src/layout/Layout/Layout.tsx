0ebe68208365b46a90607e870d83f964dcc210ce
alexander.akelyev <alexander.akelev@gmail.com>
2022-05-10 14:11:17 +0400

    Grid: Декомпозиция классов лайаута

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -12,38 +12,39 @@ const Layout: React.FC = (): React.ReactElement => (
   <>
     <Header />
     <div className="layout">
-      <div className="layout__main">
+      <main className="main">
         <Hidden smDown>
           <div className="header-main sticky-top">
             <h5 className="header-main__title">Главная</h5>
           </div>
         </Hidden>
         <Outlet />
-      </div>
+      </main>
       <Hidden mdDown>
-        <div className="layout__aside">
-          <div className="layout__aside-header sticky-top">
-            <SearchPanel />
-          </div>
-          <div className="layout__aside-content">
-            <Tweet
-              user={{
-                nickname: 'Dan',
-                author: 'dan_abramov',
-                avatarUrl:
+        <aside className="aside">
+          <div className="aside__container">
+            <div className="aside__header sticky-top">
+              <SearchPanel />
+            </div>
+            <div className="aside__content">
+              <Tweet
+                user={{
+                  nickname: 'Dan',
+                  author: 'dan_abramov',
+                  avatarUrl:
                     'https://external-preview.redd.it/yCIdRVIdKSZlwTacwwnrqx6o-ou1GNYc-WI24p_w6xE.jpg?auto=webp&s=222ab63c87addc7117b32dcbaee992fbfc60aefc',
-              }}
-              text={`“when people are rude to you, don’t forget that they might be cranky
+                }}
+                text={`“when people are rude to you, don’t forget that they might be cranky
           for an entirely unrelated reason that you can’t influence and they
           just happen to (intentionally or not) take it out on you” — me, a
           person who most definitely never does that`}
-            />
+              />
+            </div>
           </div>
-        </div>
+        </aside>
       </Hidden>
     </div>
   </>
 );
-
 
 export default Layout;
